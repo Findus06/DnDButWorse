194a35d4784d1c2291dbc382e58b04244b1da673
Vili Rask <vilirask06@gmail.com>
2023-09-04 15:29:46 +0300

    Added Names

--- DnDButWorse/Assets/CharSettingPanel.cs
+++ DnDButWorse/Assets/CharSettingPanel.cs
@@ -7,6 +7,12 @@ public class CharSettingPanel : MonoBehaviour
 {
    [SerializeField] TMPro.TMP_InputField nameTextField;
    CharacterPanel characterPanel;
+
+    [SerializeField] TMPro.TMP_Dropdown classDropDown;
+    [SerializeField] TMPro.TMP_Dropdown raceDropDown;
+
+   [SerializeField] ListOfRaces listOfRaces;
+   [SerializeField] ListOfClasses listOfClasses;
     void Awake()
     {
         characterPanel = GetComponentInParent<CharacterPanel>();
@@ -15,6 +21,26 @@ public class CharSettingPanel : MonoBehaviour
     void Start()
     {
         UpdateNameField();
+        UpdateDropDownLists();
+    }
+
+    private void UpdateDropDownLists()
+    {
+        classDropDown.ClearOptions();
+        List<string> classesOptions = new List<string>();
+        for(int i = 0; i < listOfClasses.classes.Count; i++)
+        {
+            classesOptions.Add(listOfClasses.classes[i].Name);
+        }
+        classDropDown.AddOptions(classesOptions);
+
+        raceDropDown.ClearOptions();
+        List<string> racesOptions = new List<string>();
+        for(int i = 0; i < listOfRaces.races.Count; i++)
+        {
+            racesOptions.Add(listOfRaces.races[i].Name);
+        }
+        raceDropDown.AddOptions(racesOptions);
     }
 
     private void UpdateNameField()
@@ -26,4 +52,6 @@ public class CharSettingPanel : MonoBehaviour
     {
         characterPanel.character.Name = nameTextField.text;
     }
+
+   
 }
