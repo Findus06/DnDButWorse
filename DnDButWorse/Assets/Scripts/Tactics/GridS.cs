82db1ed3b1658b3422321192856010fd7d50be1c
Findus06 <vilirask06@gmail.com>
2023-09-07 16:58:49 +0300

    stuff

--- DnDButWorse/Assets/Scripts/Tactics/GridS.cs
+++ DnDButWorse/Assets/Scripts/Tactics/GridS.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GridS : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Node[,] grid;
+    [SerializeField] int width = 25;
+    [SerializeField] int length = 25;
+    [SerializeField] float cellSize = 1f;
+
     void Start()
     {
-        
+        grid = new Node[width, length];
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDrawGizmos()
     {
-        
+        for(int y = 0; y < width; y++)
+        {
+            for(int x = 0; x < length; x++)
+            {
+                Vector3 pos = new Vector3(transform.position.x + (x * cellSize), 0f,transform.position.z + (y * cellSize));
+                Gizmos.DrawCube(pos, Vector3.one/4);
+            }
+        }
     }
 }
